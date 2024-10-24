f4a204d3e9d7ed1cd02d005c38798c9a8fcee8a0
Aleksandr Mishchenko <aleksandr.mishchenko@dxc.com>
2024-10-24 11:19:02 +0200

    feat: log

--- src/main/java/com/skoda/service/Impl/LicensesServiceImpl.java
+++ src/main/java/com/skoda/service/Impl/LicensesServiceImpl.java
@@ -70,6 +70,7 @@ public class LicensesServiceImpl implements LicensesService {
         linked.setPurchaseDate(Instant.now());
 
         linkedLicenseRepository.save(linked);
+        log.info("vehicle: {} linked: {}", vehicle, linked);
 
         return new LicenseRenewal(linked.getId(), linked.getLicence().getName(), "License successfully Renewed", true);
     }
