302899eb8af2a24a1b422aee8efb465f97c286f8
Aleksandr Mishchenko <aleksandr.mishchenko@dxc.com>
2024-10-24 14:36:11 +0200

    feat: validate

--- src/main/java/com/skoda/config/SecurityConfig.java
+++ src/main/java/com/skoda/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/health-check").permitAll()
                         .requestMatchers("/api/ivi/login/**").permitAll()
-                        .requestMatchers("/api/ivi/validate-token/**").permitAll()
+                        .requestMatchers("/api/ivi/validate/**").permitAll()
                         .requestMatchers("/api/mobile/login").permitAll()
                         .requestMatchers("/api/mobile/validate-token/**").permitAll()
                         .requestMatchers("**/swagger-ui/**", "**/v3/api-docs/**").permitAll()
